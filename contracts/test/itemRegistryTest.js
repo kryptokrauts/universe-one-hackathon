e519fce3ca03b4b6131e45bd9e5e9dd2f1514aff
Marco Walz <walz.marcoo@gmail.com>
2019-09-15 04:31:20 +0200

    working tmp state with record

--- contracts/test/itemRegistryTest.js
+++ contracts/test/itemRegistryTest.js
@@ -5,8 +5,11 @@ const sha256 = require('js-sha256').sha256;
 
 const CONTRACT_PATH = "./contracts/ItemRegistry.aes";
 
-const publicKey = "F3225AFC10CBEC2B0898375DE82B8A3DE4E0B44369BFCB7088045D74237918DFBB97E181A19F824019B6AB6FA622903B24922222E5843E1486E7BCABFDCA85B1";
-const signature = "937C68893CBBB8B538257A9965FB1E12AECD59893775CB41354FA8B5184776616C0F0C926E7F88B6A689CD84EE05E85EFEE3532B297E7C45036257A09C9DE529";
+const publicKey1 = "F3225AFC10CBEC2B0898375DE82B8A3DE4E0B44369BFCB7088045D74237918DFBB97E181A19F824019B6AB6FA622903B24922222E5843E1486E7BCABFDCA85B1";
+const signature1 = "937C68893CBBB8B538257A9965FB1E12AECD59893775CB41354FA8B5184776616C0F0C926E7F88B6A689CD84EE05E85EFEE3532B297E7C45036257A09C9DE529";
+
+const publicKey2 = "172C3391F4DD56604AFDCFF9DAB981F9079B0D38E0A9970C6439FB06B965E94CC25E24A9616DDDDB1907745E9A017644973F1B0CAAA3E18D870D95E0342571D0";
+
 const validMessage = "Something";
 const invalidMessage = "nandanistcool";
 
@@ -22,7 +25,6 @@ describe('ItemRegistry Contract', () => {
     let deployedContractOwner;
     
     let ownerKeyPair = wallets[0];
-    let recipientOneKeyPair = wallets[1];
 
     before(async () => {
         deployer = new Deployer('local', ownerKeyPair.secretKey)
@@ -35,39 +37,50 @@ describe('ItemRegistry Contract', () => {
             nativeMode: true,
             networkId: 'ae_devnet'
         });
-
-        clientRecipientOne = await Ae({
-            url: config.host,
-            internalUrl: config.internalHost,
-            compilerUrl: config.compilerUrl,
-            keypair: recipientOneKeyPair,
-            nativeMode: true,
-            networkId: 'ae_devnet'
-        });
     });
 
     describe('Deploy contract', async () => {
         it('SUCCESS deploy and initialize', async () => {
-            const limit = 1;
+            const limit = 2;
 
             deployedContractOwner = await deployer.deploy(CONTRACT_PATH, [limit]);
-            //deployedContractRecipientOne = await deployedContractOwner.from(recipientOneKeyPair.secretKey);
             contractAddress = deployedContractOwner.address;
 
             const limitResult = await deployedContractOwner.getLimit({callStatic: true});
             assert.equal(limitResult.decodedResult, limit);
-            // console.log(limitResult);
         });
     });
 
     describe('Interact with contract', async () => {
         it('should verify a valid signature', async () => {
-            let verificationResult = await deployedContractOwner.verifyItem(publicKey, sha256(validMessage), signature);
+            let verificationResult = await deployedContractOwner.verifyItem(publicKey1, sha256(validMessage), signature1);
             assert.equal(verificationResult.decodedResult, true);
         });
-        it('should not verify an invalid signature', async () => {
-            let verificationResult = await deployedContractOwner.verifyItem(publicKey, sha256(invalidMessage), signature);
+        it('should NOT verify an invalid signature', async () => {
+            let verificationResult = await deployedContractOwner.verifyItem(publicKey1, sha256(invalidMessage), signature1);
             assert.equal(verificationResult.decodedResult, false);
+        });
+        it('should be able to add an item', async () => {
+            await deployedContractOwner.addItem(publicKey1, "some metadata");
+        });
+        it('should NOT be able to add item with same publicKey again', async () => {
+            try {
+                await deployedContractOwner.addItem(publicKey1, "some metadata");
+                assert.fail(); 
+            } catch (e) {
+                console.log(e.decodedError);
+            }
+        });
+        it('should be able to add another valid item', async () => {
+            await deployedContractOwner.addItem(publicKey2, "some other metadata");
+        });
+        it('should NOT be able to add an item after limit is reached', async () => {
+            try {
+                await deployedContractOwner.addItem(publicKey2, "some other metadata");
+                assert.fail(); 
+            } catch (e) {
+                console.log(e.decodedError);
+            }
         });
     });
 });
